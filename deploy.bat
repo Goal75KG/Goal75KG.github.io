284cecb21179273fd439fac57c1f60cb635452fb
Goal75KG <jingbin.lin@outlook.com>
2024-09-05 02:16:23 +0800

    backup

--- deploy.bat
+++ deploy.bat
@@ -1,9 +1,10 @@
-echo "Start"
+@echo off
+echo Start
 git add .
-git commit -m backup
+git commit -m "backup"
 git push origin hexo
 hexo g
 hexo d
-echo "Finish"
-echo 按任意键继续
-read -n 1
+echo Finish
+echo 按任意键继续...
+pause >nul
